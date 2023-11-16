94069c1a68de67851a460194e9f4e71b831985ff
Fazal wahab <wahabdir567@gamil.com>
2023-11-16 02:08:19 -0800

    make perpect

--- src/app/components/ui/Addtocart.tsx
+++ src/app/components/ui/Addtocart.tsx
@@ -3,7 +3,6 @@ import { useDispatch } from "react-redux";
 import { cartSlice } from "@/app/store/slice/cartSlice";
 import toast from "react-hot-toast";
 import { oneProductType } from "@/app/utils/Type";
-import { useUser } from '@clerk/nextjs';
 
 interface AddToCartProps {
   product: oneProductType;  
@@ -11,22 +10,21 @@ interface AddToCartProps {
 
 const AddToCart: React.FC<AddToCartProps> = ({ product }) => {
   const dispatch = useDispatch();
-  const { user } = useUser();
 
   const addItemToCart = (quantity: number) => {
-    if (!user) {
-      toast.error("Please sign up first");
-      // You can redirect the user to the sign-up page here if needed
-    } else {
-      dispatch(cartSlice.actions.addToCart({ product, quantity }));
-      toast.success("Product added to cart");
-    }
+    dispatch(cartSlice.actions.addToCart({ product, quantity }));
+    toast.success("Product added to cart");
   };
 
   return (
-    <button onClick={() => addItemToCart(1)} className="add-to-cart-button">
-      Add to Cart
-    </button>
+    <div>
+      <button
+        onClick={() => addItemToCart(1)}
+        className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full"
+      >
+        Add to Cart
+      </button>
+    </div>
   );
 };
 
